34f8eee9f0ef839388fd44ecaea72b51ddbbb915
000VIJAY <vijay893689@gmail.com>
2022-07-13 08:50:57 +0530

    added Changes in Power of two program

--- powOfTwo/Program.cs
+++ powOfTwo/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("enter the number");
 long N = Convert.ToInt32(Console.ReadLine());
-for (int i = 1; i < N; i++)
+for (int i = 1; i <= N; i++)
 {
     double powerOfTwo = Math.Pow(2, i);
     Console.WriteLine( "Power of Two : " +powerOfTwo);
