7bb788b06360b1231b164081bdc21660416bd4da
000VIJAY <vijay893689@gmail.com>
2022-07-13 08:43:34 +0530

    Added program for Finding power of  two

---
+++ powOfTwo/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("enter the number");
+long N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i < N; i++)
+{
+    double powerOfTwo = Math.Pow(2, i);
+    Console.WriteLine( "Power of Two : " +powerOfTwo);
+}
